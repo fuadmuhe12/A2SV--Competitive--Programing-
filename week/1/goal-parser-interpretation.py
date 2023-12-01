d488c240a25ead3ecf1610492ff9ec75c7673b55
Fuad Mohammed <138528087+fuadmuhe12@users.noreply.github.com>
2023-12-01 18:03:47 +0300

    Added solution for Goal Parser Interpretation

--- week/1/goal-parser-interpretation.py
+++ week/1/goal-parser-interpretation.py
@@ -1,21 +1,5 @@
 class Solution:
     def interpret(self, command: str) -> str:
-        ans = ''
-        stack = []
-        start = 0
-        while start <len(command):
-            stack.append(command[start])
-            
-            if stack == ["G"]:
-                ans += "G"
-                stack .clear()
-            elif stack == ["(",")"]:
-                ans += "o"
-                stack.clear()
-            elif stack == ["(","a","l",")"]:
-                ans += "al"
-                stack.clear()
-            start += 1
-        return ans
+        return command.replace("()","o").replace("(al)", "al")
 
 
