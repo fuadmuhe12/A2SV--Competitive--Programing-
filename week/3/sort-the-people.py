ba6f3e172cbab00d4634127eaa230893f649f974
Fuad Mohammed <138528087+fuadmuhe12@users.noreply.github.com>
2023-12-26 17:57:41 +0300

    Added solution for Sort the People

--- week/3/sort-the-people.py
+++ week/3/sort-the-people.py
@@ -1,12 +1,19 @@
 class Solution:
     def sortPeople(self, n: List[str], h: List[int]) -> List[str]:
-        dic = defaultdict(list)
-        ans = []
-        for i in range(len(n)):
-            dic[h[i]].append(n[i])
-        for i in sorted(dic.keys(), reverse =True):
-            ans.extend(dic[i])
-        return  ans
+        #slectection sort
+        s = 0
+        while s < len(n):
+            l = s+ 1
+            while l < len(n):
+                if h[l] > h[s]:
+                    h[l], h[s] = h[s], h[l]
+                    n[l], n[s] = n[s], n[l]
+
+                l += 1
+            s+= 1
+        return n
+        
+
 
 
 
