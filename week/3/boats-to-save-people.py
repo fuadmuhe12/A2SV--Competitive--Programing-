1bd24b900564a37821293bd17abf78668f7b0b80
Fuad Mohammed <138528087+fuadmuhe12@users.noreply.github.com>
2024-01-09 10:51:26 +0300

    Added solution for Boats to Save People

--- week/3/boats-to-save-people.py
+++ week/3/boats-to-save-people.py
@@ -1,17 +1,20 @@
 class Solution:
     def numRescueBoats(self, people: List[int], limit: int) -> int:
+        '''
+        1.sort the poeple based on therir weight and have left and right pointer while the  right is >= left
+        if the sum of two is equal to the weight move insed increase the bot else move the right only
+                    '''
+        l = 0
+        r =  len(people)-1
         people.sort()
-        start = 0
-        end = len(people) -1
-        bot = 0
-        while end >= start:
-            if people[end] + people[start] <= limit:
-                bot += 1
-                end -= 1
-                start += 1
+        tot = 0
+        while r >=l:
+            if people [r] + people[l] <= limit:
+                l += 1
+                r -= 1
             else:
-                bot +=1
-                end -= 1
-        return bot
+                r -= 1
+            tot += 1
+        return tot
 
 
