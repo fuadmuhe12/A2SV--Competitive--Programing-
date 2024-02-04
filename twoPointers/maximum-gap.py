b4330c330af8015f2e5fb395dc7dc1e60788b26a
Fuad Mohammed <138528087+fuadmuhe12@users.noreply.github.com>
2024-02-04 22:57:05 +0300

    Added solution for Maximum Gap

---
+++ twoPointers/maximum-gap.py
@@ -0,0 +1,10 @@
+class Solution:
+    def maximumGap(self, nums: List[int]) -> int:
+        nums.sort()
+        mx = 0
+        if len(nums) < 2:
+            return 0
+        else:
+            for i in range(1, len(nums)):
+                mx = max(mx, nums[i] -nums[i-1])
+            return mx
