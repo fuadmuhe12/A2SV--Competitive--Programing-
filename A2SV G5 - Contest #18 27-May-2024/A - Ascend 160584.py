4ef1c46772039b3da83f808c9e3b98b30ec4e87c
Fuad Mohammed <138528087+fuadmuhe12@users.noreply.github.com>
2024-05-27 19:31:12 +0300

    Created 'A - Ascend' by A2SV Hub

---
+++ A2SV G5 - Contest #18 27-May-2024/A - Ascend 160584.py
@@ -0,0 +1,15 @@
+# Problem: A - Ascend - https://codeforces.com/gym/526229/problem/A
+
+t= int(input())
+lis = list(map(int, input().split()))
+
+cur  = 0
+mx = 0
+start = 0
+for i in range(t):
+    if lis[i] <= cur:
+        
+        start = i
+    cur = lis[i]
+    mx =  max(mx, i - start +1)
+print(mx)
